efe4391e0f26bf02df057c58360d5adc2d353050
LuisDev18 <brian@orbita.pe>
2023-09-20 11:56:31 -0500

    axios-async-await

--- js/ajax.js
+++ js/ajax.js
@@ -45,19 +45,19 @@
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => {
       console.log(res);
-      return res.ok? res.json() : Promise.reject(res);
+      return res.ok ? res.json() : Promise.reject(res);
     })
     .then((json) => {
       console.log(json);
-      json.forEach((el) =>{
+      json.forEach((el) => {
         const $li = document.createElement("li");
-        $li.innerHTML= `${el.name} -- ${el.email} -- ${el.phone}`;
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
         $fragment.appendChild($li);
-      }) 
-      $fetch.appendChild($fragment)
+      });
+      $fetch.appendChild($fragment);
     })
     .catch((err) => {
-      console.log("Estamos en el catch",err);
+      console.log("Estamos en el catch", err);
       let message = err.statusText || "Ocurrio un error";
       $fetch.innerHTML = `Error ${err.status}: ${message}`;
     })
@@ -66,4 +66,106 @@
         "Esto se ejecutara independientemente del resultado de la Promesa Fetch"
       );
     });
+})();
+
+//function anonima autoejecutable utilizando la API Fetch + Async-Await
+(() => {
+  const $fetchAsync = document.getElementById("fetch-async");
+  const $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/users");
+      json = await res.json();
+      console.log(res, json);
+
+      //if(!res.ok) throw new Error("Ocurrio un error al solicitar los datos");
+      if (!res.ok)
+        throw {
+          status: res.status,
+          statusText: res.statusText,
+        };
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {
+      console.log(err);
+      let message = err.statusText || "Ocurrio un error";
+      $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+    } finally {
+      console.log(
+        "Esto se ejecutara independientemente del resultado de la Promesa Fetch"
+      );
+    }
+  }
+
+  getData();
+})();
+
+
+//Funcion anonima autoejecutable utilizando la libreria de AXIOS
+(()=>{
+   const $axios = document.getElementById("axios");
+   const $fragment = document.createDocumentFragment();
+  
+   axios.get("https://jsonplaceholder.typicode.com/user")
+   .then((res)=>{
+    console.log(res);
+    let json = res.data;
+
+    json.forEach((el) => {
+      const $li = document.createElement("li");
+      $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+      $fragment.appendChild($li);  
+    });
+      $axios.appendChild($fragment);
+   })
+   .catch((err)=>{
+     console.log("Estamos en el catch",err.response);
+     let message = err.responseText || "Ocurrio un error";
+     $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+   })
+   .finally(()=>{
+    console.log("Esto se ejecutara independiente del resultado de AXIOS")
+   });
+
+})();
+
+
+//Funcion anonima autoejecutable utilizando la libreria de AXIOS + Async-Await
+(()=>{
+    const $axiosAsync = document.getElementById("axios-async");
+    const $fragment = document.createDocumentFragment();
+
+   async function getData(){
+    try{
+    let res = await axios.get("https://jsonplaceholder.typicode.com/users")
+    json = await res.data;
+    console.log(res,json);
+
+
+    json.forEach((el)=>{
+      const $li = document.createElement("li");
+      $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+      $fragment.appendChild($li);
+    })
+
+    $axiosAsync.appendChild($fragment);
+
+    }catch(err){
+       console.log(err.response);
+       let message = err.response.statusText || "Ocurrio un error";
+       $axiosAsync.innerHTML = `Error ${err.response.status} : ${message}`;
+    } finally{
+      console.log("Esto se ejecutara independientemente")
+    }
+  
+   }
+
+   getData();
+    
 })();
