fac8a59b3b05e978270d7ebd0d3c0ce8e8bab378
LuisDev18 <brian@orbita.pe>
2024-03-15 11:00:56 -0500

    feat:Ejercicios de expresiones regulares

--- Ejercicios/EjercicioFort.js
+++ Ejercicios/EjercicioFort.js
@@ -29,6 +29,52 @@ console.log(descuent(100,50));
 
 //Ejercicios de Expresiones Regulares
 //17: Desarrolle una funcion que devuelva la cantidad de vocales y consontantes de una palabra.
+const contadorVocales = (palabra="")=>{
+    if(!palabra) return console.log("Ingrese una palabra");
+    if(typeof palabra !== "string") return console.log("Ingrese una cadena de texto");
 
+    let contadorVocales=0;
+    let contadorConsonantes=0;
+    let expReg=/[aeiouAEIOU]/;
+    let expRegCons=/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/;
 
+    for(let i=0;i<palabra.length;i++){
 
+        if(expReg.test(palabra[i]) === true){
+            contadorVocales++;
+           }
+
+         if(expRegCons.test(palabra[i]) === true){
+           contadorConsonantes++;
+         }  
+
+    }
+
+    return console.log(`La palabra ${palabra} tiene: ${contadorVocales} vocales y tiene ${contadorConsonantes} consonantes`);
+   
+}
+
+contadorVocales("hola me llamo Luis");
+
+//18: Desarrolle una funcion que cuando se ingresa un Nombre sea un texto valido
+const validateName = (nombre="")=>{
+    if(!nombre) return console.log("Ingrese un nombre")
+    expRegular=/^[A-Z][a-z]*( [A-Z][a-z]*)*$/;
+    
+    return (expRegular.test(nombre)===true?console.log("Nombre correcto"):console.log(`El nombre no esta bien escrito ${nombre}`));
+}
+
+validateName("Luis Pepinillo Rick");
+
+//19:Desarrolle una funcion que permita que un correo sea valido
+const checkEmail = (email="")=>{
+    if(!email) return console.log("Ingrese un email");
+    let expRegular = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if(expRegular.test(email)===true){
+      return  console.log("Correo valido");
+    }else{
+      return  console.log("Correo invalido");
+    }
+}
+
+checkEmail("luis_18@gmail.com");
