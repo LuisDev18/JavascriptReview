f0c002cf29cd5146f1c5a40073a668edbcb49f81
LuisDev18 <brian@orbita.pe>
2024-04-01 16:26:38 -0500

    feat:Objeto JSON con JS

---
+++ DataType/Json.js
@@ -0,0 +1,21 @@
+/*PARSE: Convierte un JSON a una expresion JS*/ 
+console.log("*** JSON.parse ***");
+console.log(JSON.parse("{}"));
+console.log(JSON.parse("[1,2,3]"));
+console.log(JSON.parse("true"));
+console.log(JSON.parse("false"));
+console.log(JSON.parse("19"));
+console.log(JSON.parse('"Hola Mundo"'));
+console.log(JSON.parse("null"));
+
+/*stringify(): Convierte un js en un JSON*/ 
+console.log("*** JSON.stringify ***");
+console.log(JSON.stringify({}));
+console.log(JSON.stringify([1, 2, 3]));
+console.log(JSON.stringify(true));
+console.log(JSON.stringify(false));
+console.log(JSON.stringify(19));
+console.log(JSON.stringify("Hola Mundo"));
+console.log(JSON.stringify(null));
+console.log(JSON.stringify(undefined));
+console.log(JSON.stringify({ x: 2, y: 3 }));
